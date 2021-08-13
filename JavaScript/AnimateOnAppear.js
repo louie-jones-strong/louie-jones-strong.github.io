b0af72766bf651166afb667bcfc4ef51c73127d6
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2021-08-13 23:36:01 +0100

    update

---
+++ JavaScript/AnimateOnAppear.js
@@ -0,0 +1,23 @@
+window.addEventListener("scroll", Reveal);
+
+function Reveal()
+{
+	var reveals = document.querySelectorAll(".reveal");
+
+	for(var i = 0; i < reveals.length; i++)
+	{
+
+		var windowHeight = window.innerHeight;
+		var revealTop = reveals[i].getBoundingClientRect().top;
+		var revealPoint = 150;
+
+		if(revealTop < windowHeight - revealPoint)
+		{
+			reveals[i].classList.add("shown");
+		}
+		else
+		{
+			reveals[i].classList.remove("shown");
+		}
+	}
+}
