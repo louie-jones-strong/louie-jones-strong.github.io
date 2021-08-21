ecc4f05cc845ad40ef202e8af6d5c943278e0294
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2021-08-21 17:18:32 +0100

    hook up to theme toggle icon

--- JavaScript/ToggleTheme.js
+++ JavaScript/ToggleTheme.js
@@ -15,22 +15,33 @@ function ToggleTheme()
 {
 	var currentTheme = localStorage.getItem("theme");
 
-	SetTheme(currentTheme == "light")
+	SetTheme(currentTheme == "light");
 }
 
 
 function SetTheme(isDarkTheme)
 {
+	var darkModeIcon = document.getElementById("darkModeIcon");
+	var lightModeIcon = document.getElementById("lightModeIcon");
+
 	if (isDarkTheme)
 	{
 		document.body.classList.remove("light");
 		document.body.classList.add("dark");
+
+		darkModeIcon.classList.remove("hide");
+		lightModeIcon.classList.add("hide");
+
 	}
 	else
 	{
 		document.body.classList.add("light");
 		document.body.classList.remove("dark");
+
+		darkModeIcon.classList.add("hide");
+		lightModeIcon.classList.remove("hide");
 	}
+;
 	localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
 }
 
