e4b8db6dedcfab711f56815e0c740ee08a681b22
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2021-08-21 21:56:34 +0100

    update loading

--- JavaScript/Loader.js
+++ JavaScript/Loader.js
@@ -1,3 +1,33 @@
-$(window).on("load",function(){
+$(window).on("load", Loaded);
+
+HideContent();
+
+
+function Loaded()
+{
+	setTimeout(ShowContent, 500);
+}
+
+function HideContent()
+{
+	var contents = document.getElementsByClassName("content");
+
+	for (let index = 0; index < contents.length; index++)
+	{
+		const item = contents[index];
+		item.classList.add("hide");
+	}
+}
+
+function ShowContent()
+{
 	$("#loaderHolder").fadeOut("slow");
-});
+
+	var contents = document.getElementsByClassName("content");
+
+	for (let index = 0; index < contents.length; index++)
+	{
+		const item = contents[index];
+		item.classList.remove("hide");
+	}
+}
